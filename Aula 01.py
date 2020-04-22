a37168317ca14ebca72e7c7afe4acb6400ec1cc8
brunoamaia94 <brunoamaia94@users.noreply.github.com>
2020-04-22 11:19:46 -0300

    Aula 1 - Desafios 5 e 6

--- Aula 01.py
+++ Aula 01.py
@@ -1,6 +1,8 @@
-import pandas as pd     # Imortar a biblioteca "pandas"
-import math
 import matplotlib.pyplot as plt
+import pandas as pd     # Imortar a biblioteca "pandas"
+
+import math
+
 
 ### Importar / Ler os arquivos brutos ###
 filmes = pd.read_csv("B:\Programação\Quarentena_Dados\dados\movies.csv")    # Existem outros read: read_pdf, read_xls, ...
@@ -64,9 +66,10 @@ print(filmes_com_media.describe())
 filmes_sem_nota = filmes_com_media.query('nota.isna()')    # Forma de separar os filmes sem nota
 # filmes_sem_nota = filmes_com_media.query('nota.isnull()', engine='python')  # Forma  que funcionou no colab.research.google
 filmes_s_n = filmes_sem_nota['titulo'].values.tolist()        # Transforma a coluna de Titulos em uma lista
+n = len(filmes_s_n)
 print('\n\n*#*#*#*#  Desafio 1  #*#*#*#*')
-print('Encotrar os filmes que não receberam nenhuma nota')
-print('Filmes sem avaliação: \n')
+print('Encotrar os filmes que não receberam nenhuma nota.')
+print(f'Foram encontrados {n} filmes sem avaliação, sendo eles:')
 print(*filmes_s_n, sep = "\n")              # Imprimir cada valor em uma linha
 
 
@@ -75,8 +78,8 @@ print(*filmes_s_n, sep = "\n")              # Imprimir cada valor em uma linha
 # filmes_com_media.columns = ['filmeId', 'titulo', 'generos', 'media']    # Quando sabe a posição exata e/ou quer mudar mais que um nome
 filmes_com_media.rename(columns={'nota':'media'}, inplace=True)         # Modifcar do nome "A" para o "B"
 print('\n\n*#*#*#*#  Desafio 2  #*#*#*#*')
-print('Renomear a coluna "nota", para "media"')
-print('Tabela com o nome da coluna modificada: \n')
+print('Renomear a coluna "nota", para "media".')
+print('Segue a Tabela com o nome da coluna modificada:')
 print(filmes_com_media)
 filmes_com_media.head(5)
 # filmes_com_media.columns = ['filmeId', 'media']
@@ -85,15 +88,14 @@ filmes_com_media.head(5)
 
 ###         Desafio 3         ###
 # Colocar a quantidade de avaliações que cada filme recebeu
-
 contar_votos = pd.DataFrame({'filmeId':avaliacoes['filmeId']})     # Criar DataFrame com os filmeId
 contar_votos['votos'] = contar_votos                              # Duplicar apra manter a "chave" (filmeId). Pois erá utilizada para linkar com a tabela de nomes
 contar_votos = contar_votos.groupby('filmeId')['votos'].count()   # Contar quantas vezes apareceu cada Id (como ficou com 3 colunas, a 1ª mostra o Id do filme)
 contar_votos = filmes.join(contar_votos, on='filmeId')     # Insere a coluna de filmes (nome dos filmes), na tabela de cotar votos
 contar_votos = contar_votos.drop('filmeId', 1)                             # Remover uma coluna ('nome', eixo: 0 para linhas (x) ou 1 para colunas (y))
 print('\n\n*#*#*#*#  Desafio 3  #*#*#*#*')
-print('Colocar a quantidade de avaliações que cada filme recebeu')
-print('Quantidade de voto dos filmes: ')
+print('Colocar a quantidade de avaliações que cada filme recebeu.')
+print('Tabela com a quantidade de voto dos filmes: ')
 print(contar_votos)
 
 
@@ -101,37 +103,62 @@ print(contar_votos)
 # Arredondar as casas decimais das médias
 filmes_com_media['media'] = filmes_com_media['media'].round(decimals=2)
 print('\n\n*#*#*#*#  Desafio 4  #*#*#*#*')
-print('Arredondar para duas casas decimais as médias')
+print('Arredondar para duas casas decimais as médias.')
 print('Tabela com os valores arredondados: ')
 print(filmes_com_media)
 
 
 ###         Desafio 5         ###
 # Verificar quantos são os gêneros únicos e quais são (esse aqui o bixo pega kkk)
+#generos_unicos = pd.DataFrame(filmes.genero.str.split('|').tolist()).stack().unique()   # Forma direta de chamar o comando
+        # O comando foi "quebrado" para ser mais facil entender
+#generos_contados = filmes['genero'].str.get_dummies('|').sum()              # Forma direta para separar e contar os generos
+    # .str habilita as ferramentas de String. .get_dumies('*'), separa no caracter '*', cada item separado se torna uma nova coluna. .sum faz o somatório, neste caso somou os valores das colunas
+    # Fazer "na unha"
 generos = filmes['genero']  # Pega a coluna de Generos
-# .unique()         # Verifica os generos unicos
-#print(generos_unicos)
-# print('\n\n*#*#*#*#  Desafio 5  #*#*#*#*')
-# print('Arredondar para duas casas decimais as médias')
-# print('Tabela com os valores arredondados: ')
-# print()
+generos = generos.str.split('|')          # Separar os generos da mesma linha (que estão perarados por "|") o .str Serve para trabalhar com string
+generos = generos.tolist()                # Tornar uma lista
+generos = pd.DataFrame(generos).stack()   # Remodela o Dataframe. Coloca cada estilo em uma linha nova (essas linhas novas são criadas dentro de cada filme)
+generos_unicos = generos.unique()                # Verifica os generos unicos
+generos_unicos.sort()                                   # Ordenar de forma alfabetica os generos
+
+#generos_unicos = pd.DataFrame(generos_unicos, columns=['genero'])   # Coloca o nome de "genero" na coluna e reeestrutura como um Dataframe (pois tinha tornado uma lista)
+n = len(generos_unicos)     # Quantidade de generos
+print('\n\n*#*#*#*#  Desafio 5  #*#*#*#*')
+print('Verificar quantos são os gêneros únicos e quais são (esse aqui o bixo pega kkk)')
+print(f'Foram encontrados {n} generos, eles são os seguintes: ')
+#print(generos_unicos)                           # Imprimir como Dataframe
+print(*generos_unicos, sep = "\n")              # Imprimir cada valor em uma linha
+
 
 ###         Desafio 6         ###
 # Quantas vezes aparece cada gênero
+# generos_quantidade, dum = filmes(data, 'genres', genre_labels)
+contar_generos = filmes['genero'].str.get_dummies('|').sum()              # Usando o método direto
+                # .str habilita as ferramentas de String. .get_dumies('*'), separa no caracter '*', cada item separado se torna uma nova coluna. .sum faz o somatório, neste caso somou os valores das colunas
+    # método mais trabalhoso
+# contar_generos = pd.DataFrame(generos, columns=['genero'])      # Importar o Dataframe remodeado com os generos
+# contar_generos['contador'] = contar_generos['genero']           # Duplicar coluna para fazer a contagem
+# contar_generos = contar_generos.groupby('genero')['contador'].count()   # Contador
 
-# print('\n\n*#*#*#*#  Desafio 6  #*#*#*#*')
-# print('Arredondar para duas casas decimais as médias')
-# print('Tabela com os valores arredondados: ')
-# print()
+print('\n\n*#*#*#*#  Desafio 6  #*#*#*#*')
+print('Arredondar para duas casas decimais as médias')
+print('Tabela com os valores arredondados: ')
+print(contar_generos)
 
 
 ###         Desafio 7         ###
 # Criar um gráfico para o "Desafio 6". Pode ser de barra
 
-# print('\n\n*#*#*#*#  Desafio 7  #*#*#*#*')
-# print('Arredondar para duas casas decimais as médias')
+
+print('\n\n*#*#*#*#  Desafio 7  #*#*#*#*')
+print('Criar um gráfico para o "Desafio 6". Pode ser de barra')
 # print('Tabela com os valores arredondados: ')
 # print()
+contar_generos.describe()
+
+plt('contar_generos')
+pause = input('digite a')
 
 
 
