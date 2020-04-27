d04eda89b03aede3989898c8a8d348a9563a6fd7
brunoamaia94 <brunoamaia94@users.noreply.github.com>
2020-04-27 11:49:40 -0300

    Remover linhas com nota menor que 100

--- Aula 04.py
+++ Aula 04.py
@@ -15,10 +15,44 @@ enem = pd.read_csv('B:\Programação\Quarentena_Dados\dados\enem_sample_2018_432
 colunas_de_notas = enem[['NU_NOTA_CN', 'NU_NOTA_CH', 'NU_NOTA_LC', 'NU_NOTA_MT', 'NU_NOTA_REDACAO']]    # Pegar as colunas de notas
 notas = colunas_de_notas.dropna()                                                                       # Remove quem não possui nota em alguma prova
 notas.columns = ['cienc_naturais', 'cienc_humanas', 'linguagem_codigo', 'matematica','redacao']
-a = len(notas)
+n = len(notas)
 notas = notas.drop_duplicates()
-print(f'Foram removidas {len(colunas_de_notas)-a} linhas de dados "em branco"')
-print(f'Foram removidas {a-len(notas)} linhas de dados duplicados')
+print(f'Foram removidas {len(colunas_de_notas)-n} linhas de dados "em branco"')
+print(f'Foram removidas {n-len(notas)} linhas de dados duplicados')
+
+
+# Remover as linhas com notas menor que 100 (por Parecer que são outliers)
+f = 100 # filto do valor mínimo
+notas_uteis = notas[ (notas.linguagem_codigo > f) &         # Fazer em Apenas um comando
+                     (notas.cienc_humanas > f) &
+                     (notas.cienc_naturais > f) &
+                     (notas.matematica > f) &
+                     (notas.redacao > f)      ]
+print(f'Dataframe original possui {n} linhas.] ')
+print(f'Foram removidas {n - len(notas_uteis)} linhas. Ao remover as notas menores que {f}')
+print(f'Portanto, temos agora {len(notas_uteis)} linhas')
+
+# ## Fazer em vários comandos (tem como saber quanto foi removido em cada área):
+
+# notas_uteis = notas[notas.linguagem_codigo > 100]
+# t1 =len(notas_uteis)
+# notas_uteis = notas_uteis[notas_uteis.cienc_humanas > 100]
+# t2 =len(notas_uteis)
+# notas_uteis = notas_uteis[notas_uteis.cienc_naturais > 100]
+# t3 =len(notas_uteis)
+# notas_uteis = notas_uteis[notas_uteis.matematica > 100]
+# t4 =len(notas_uteis)
+# notas_uteis = notas_uteis[notas_uteis.redacao > 100]
+# t5 =len(notas_uteis)
+# print(f'Dataframe original possui {n} linhas.] ')
+# print(f'No total, foram removidas {n - len(notas_uteis)}')
+# print(f'Sendo: {n - t1} de Linguagem, {t1 - t2} de Ciencias Humanas,\n{t2 - t3} de Ciencias Naturais, {t3 - t4} de Matemática e {t4 - t5} de Redação.')
+# print(f'Tamamnhos: T1={t1}, T2={t2}, T3={t3}, T4={t4}, T5={t5}')
+
+
+
+
+
 corr = notas.corr()
 print('Analised e Correlação')
 print(corr)
