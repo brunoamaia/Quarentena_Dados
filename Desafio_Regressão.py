388d6db80e72535e02a30f2e7192a4f1847a47c4
brunoamaia94 <brunoamaia94@users.noreply.github.com>
2020-04-28 18:58:35 -0300

    Testes no SVR

--- Desafio_Regressão.py
+++ Desafio_Regressão.py
@@ -23,21 +23,22 @@ np.random.seed(43267)       # Fixa a geração dos termos aleatórios
 import time
 start = time.time()
 
-
-# https://github.com/tgcsantos/quaretenadados/blob/master/DADOS_TREINO.csv?raw=true
-# https://raw.githubusercontent.com/tgcsantos/quaretenadados/master/DADOS_TREINO.csv
+# Links para os arquivos
+# Dados para treino: https://raw.githubusercontent.com/tgcsantos/quaretenadados/master/DADOS_TREINO.csv
+# Dados para Tesre:  https://raw.githubusercontent.com/tgcsantos/quaretenadados/master/DADOS_TESTE.csv
 dados_treino = pd.read_csv('B:\Programação\Quarentena_Dados\dados\desafio\DADOS_TREINO.csv')
 dados_teste = pd.read_csv('B:\Programação\Quarentena_Dados\dados\desafio\DADOS_TESTE.csv')
 dados_desafio = pd.read_csv('B:\Programação\Quarentena_Dados\dados\desafio\DESAFIOQT.csv')
-print(f'Dados do Treino: {dados_treino.shape}')
-print(f'Dados do Teste: {dados_teste.shape}')
-print(f'Dados do Desafio: {dados_desafio.shape}')
+print(f'Dados do Treino: \t{len(dados_treino)}')
+print(f'Dados do Teste: \t{len(dados_teste)}')
+print(f'Dados do Desafio: \t{len(dados_desafio)}')
 
 
+sns.pairplot(dados_treino)
+plot.show()
 
 coluna_label = 'NU_NOTA_LC'
 coluna_features = ['NU_NOTA_CN', 'NU_NOTA_CH', 'NU_NOTA_MT', 'NU_NOTA_REDACAO']
-
 x_treino = dados_treino[coluna_features].to_numpy()
 y_treino = dados_treino[coluna_label].to_numpy()
 x_teste = dados_teste[coluna_features].to_numpy()
@@ -48,16 +49,46 @@ y_teste = dados_teste[coluna_label].to_numpy()
 ################################            Treino da inteligencia              ################################
 resultados  = {}    # Criar um Dicionário para armazenar a qualidade dos testes
 
-print('\nModelo - Linear SVR')              # Linear SVR
+print('\nModelo - SVR')                     # SVR - Sigmoid
 a = time.process_time()
-modelo_svrl = LinearSVR(max_iter=1000)
-modelo_svrl = modelo_svrl.fit(x_treino, y_treino)
-predicoes_svrl = modelo_svrl.predict(x_teste)
-qualidade_svrl0 = mean_squared_error(y_teste, predicoes_svrl)
-del modelo_svrl, predicoes_svrl
-resultados['lin. SVR0: \t']=qualidade_svrl0
+modelo_svr = SVR(kernel='sigmoid')
+modelo_svr = modelo_svr.fit(x_treino, y_treino)
+predicoes_svr = modelo_svr.predict(x_teste)
+qualidade_svr0 = mean_squared_error(y_teste, predicoes_svr)
+del modelo_svr, predicoes_svr
+resultados['Sigmoid: \t\t']=qualidade_svr0
 print(f'Tempo gasto: {time.process_time()- a} s')
 
+print('\nModelo - SVR')                     # SVR - precomputed
+a = time.process_time()
+modelo_svr = SVR(kernel='precomputed')
+modelo_svr = modelo_svr.fit(x_treino, y_treino)
+predicoes_svr = modelo_svr.predict(x_teste)
+qualidade_svr0 = mean_squared_error(y_teste, predicoes_svr)
+del modelo_svr, predicoes_svr
+resultados['Precomputed: \t\t']=qualidade_svr0
+print(f'Tempo gasto: {time.process_time()- a} s')
+
+print('\nModelo - SVR')                     # SVR - precomputed - poly
+a = time.process_time()
+modelo_svr = SVR(kernel='poly')
+modelo_svr = modelo_svr.fit(x_treino, y_treino)
+predicoes_svr = modelo_svr.predict(x_teste)
+qualidade_svr0 = mean_squared_error(y_teste, predicoes_svr)
+del modelo_svr, predicoes_svr
+resultados['Poly: \t\t']=qualidade_svr0
+print(f'Tempo gasto: {time.process_time()- a} s')
+
+# print('\nModelo - Linear SVR')              # Linear SVR
+# a = time.process_time()
+# modelo_svrl = LinearSVR(max_iter=1000)
+# modelo_svrl = modelo_svrl.fit(x_treino, y_treino)
+# predicoes_svrl = modelo_svrl.predict(x_teste)
+# qualidade_svrl0 = mean_squared_error(y_teste, predicoes_svrl)
+# del modelo_svrl, predicoes_svrl
+# resultados['lin. SVR0: \t']=qualidade_svrl0
+# print(f'Tempo gasto: {time.process_time()- a} s')
+#
 # print('\nModelo - SVR')                     # SVR
 # a = time.process_time()
 # modelo_svr = SVR()
@@ -67,68 +98,70 @@ print(f'Tempo gasto: {time.process_time()- a} s')
 # del modelo_svr, predicoes_svr
 # resultados['SVR0: \t\t']=qualidade_svr0
 # print(f'Tempo gasto: {time.process_time()- a} s')
-
-print('\nModelo - Ávore de Decisão')          # Ávore de Decisão
-a = time.process_time()
-modelo_dt = DecisionTreeRegressor()
-modelo_dt = modelo_dt.fit(x_treino, y_treino)
-predicoes_dt = modelo_dt.predict(x_teste)
-qualidade_dt0 = mean_squared_error(y_teste, predicoes_dt)
-del modelo_dt, predicoes_dt
-resultados['DT0: \t\t']=qualidade_dt0
-print(f'Tempo gasto: {time.process_time()- a} s')
-
-print('\nModelo - Falso (média)')             # Média
-a = time.process_time()
-modelo_falso = DummyRegressor()
-modelo_falso = modelo_falso.fit(x_treino, y_treino)
-predicoes_falsas = modelo_falso.predict(x_teste)
-qualidade_media0 = mean_squared_error(y_teste, predicoes_falsas)
-del modelo_falso, predicoes_falsas
-resultados['Media0: \t']=qualidade_media0
-print(f'Tempo gasto: {time.process_time()- a} s')
-
-print('\nModelo - Falso (mediana)')           # Mediana
-a = time.process_time()
-modelo_falso = DummyRegressor(strategy="median")
-modelo_falso = modelo_falso.fit(x_treino, y_treino)
-predicoes_falsas = modelo_falso.predict(x_teste)
-qualidade_mediana0 = mean_squared_error(y_teste, predicoes_falsas)
-del modelo_falso, predicoes_falsas
-resultados['Mediana0: \t']=qualidade_mediana0
-print(f'Tempo gasto: {time.process_time()- a} s')
-
-
-
-
-
-# ##################################       Remover as notas abaxido de 100      ######################################
-# del x, y, x_treino, y_treino, x_teste, y_teste
-# f = 100 # filto do valor mínimo
-# notas_uteis = notas[ (notas.linguagem_codigo > f) &         # Fazer em Apenas um comando
-#                      (notas.cienc_humanas > f) &
-#                      (notas.cienc_naturais > f) &
-#                      (notas.matematica > f) &
-#                      (notas.redacao > f)      ]
 #
-# x = notas_uteis[['cienc_naturais', 'cienc_humanas', 'matematica', 'redacao']]
-# y = notas_uteis['linguagem_codigo']
-# x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, random_state=326784)      # Separa a amostra em elementos de treino e de teste
+# print('\nModelo - Ávore de Decisão')          # Ávore de Decisão
+# a = time.process_time()
+# modelo_dt = DecisionTreeRegressor()
+# modelo_dt = modelo_dt.fit(x_treino, y_treino)
+# predicoes_dt = modelo_dt.predict(x_teste)
+# qualidade_dt0 = mean_squared_error(y_teste, predicoes_dt)
+# del modelo_dt, predicoes_dt
+# resultados['DT0: \t\t']=qualidade_dt0
+# print(f'Tempo gasto: {time.process_time()- a} s')
 #
-# print('Dataframe sem as notas abaixo de 100')
-# print(f'Dados para treino (x e y): x = {x_treino.shape} e y = {y_treino.shape}')
-# print(f'Dados para teste (x e y): x = {x_teste.shape} e y = {y_teste.shape}')
+# print('\nModelo - Falso (média)')             # Média
+# a = time.process_time()
+# modelo_falso = DummyRegressor()
+# modelo_falso = modelo_falso.fit(x_treino, y_treino)
+# predicoes_falsas = modelo_falso.predict(x_teste)
+# qualidade_media0 = mean_squared_error(y_teste, predicoes_falsas)
+# del modelo_falso, predicoes_falsas
+# resultados['Media0: \t']=qualidade_media0
+# print(f'Tempo gasto: {time.process_time()- a} s')
 #
-#
-#
-#
-#
+# print('\nModelo - Falso (mediana)')           # Mediana
+# a = time.process_time()
+# modelo_falso = DummyRegressor(strategy="median")
+# modelo_falso = modelo_falso.fit(x_treino, y_treino)
+# predicoes_falsas = modelo_falso.predict(x_teste)
+# qualidade_mediana0 = mean_squared_error(y_teste, predicoes_falsas)
+# del modelo_falso, predicoes_falsas
+# resultados['Mediana0: \t']=qualidade_mediana0
+# print(f'Tempo gasto: {time.process_time()- a} s')
+
+
+
+
+
+##################################       Remover as notas abaxido de 100      ######################################
+del x_treino, y_treino
+f = 100 # filto do valor mínimo
+notas = dados_treino[ (dados_treino.NU_NOTA_LC > f) &         # Fazer em Apenas um comando
+                            (dados_treino.NU_NOTA_CH > f) &
+                            (dados_treino.NU_NOTA_CN > f) &
+                            (dados_treino.NU_NOTA_MT > f) &
+                            (dados_treino.NU_NOTA_REDACAO > f)]
+
+x_treino = notas[coluna_features].to_numpy()
+y_treino = notas[coluna_label].to_numpy()
+
+sns.pairplot(notas)
+plot.show()
+
+print('\nDados após remover notas abaixo de 100 (supondo que são Outliers')
+print(f'foram removidas {len(dados_treino) - len(notas)} linhas com notas abaixo de 100')
+print(f'Dados do Treino = {len(x_treino)} e {len(y_treino)}')
+print(f'Dados do Teste = {len(x_teste)} e {len(y_teste)}')
+
+
 # print('\nModelo - Linear SVR')              # Linear SVR
+# a = time.process_time()
 # modelo_svrl = LinearSVR(max_iter=1000)
 # modelo_svrl = modelo_svrl.fit(x_treino, y_treino)
 # predicoes_svrl = modelo_svrl.predict(x_teste)
-# qualidade_svrl1 = mean_squared_error(y_teste, predicoes_svrl)
+# qualidade_svrl0 = mean_squared_error(y_teste, predicoes_svrl)
 # del modelo_svrl, predicoes_svrl
+# resultados['lin. SVR1: \t']=qualidade_svrl0
 # print(f'Tempo gasto: {time.process_time()- a} s')
 #
 # print('\nModelo - SVR')                     # SVR
@@ -136,37 +169,40 @@ print(f'Tempo gasto: {time.process_time()- a} s')
 # modelo_svr = SVR()
 # modelo_svr = modelo_svr.fit(x_treino, y_treino)
 # predicoes_svr = modelo_svr.predict(x_teste)
-# qualidade_svr1 = mean_squared_error(y_teste, predicoes_svr)
+# qualidade_svr0 = mean_squared_error(y_teste, predicoes_svr)
 # del modelo_svr, predicoes_svr
+# resultados['SVR1: \t\t']=qualidade_svr0
 # print(f'Tempo gasto: {time.process_time()- a} s')
 #
-# print('Modelo - Ávore de Decisão')          # Ávore de Decisão
+# print('\nModelo - Ávore de Decisão')          # Ávore de Decisão
 # a = time.process_time()
 # modelo_dt = DecisionTreeRegressor()
 # modelo_dt = modelo_dt.fit(x_treino, y_treino)
 # predicoes_dt = modelo_dt.predict(x_teste)
-# qualidade_dt1 = mean_squared_error(y_teste, predicoes_dt)
+# qualidade_dt0 = mean_squared_error(y_teste, predicoes_dt)
 # del modelo_dt, predicoes_dt
+# resultados['DT1: \t\t']=qualidade_dt0
 # print(f'Tempo gasto: {time.process_time()- a} s')
 #
-# print('Modelo - Falso (média)')             # Média
+# print('\nModelo - Falso (média)')             # Média
 # a = time.process_time()
 # modelo_falso = DummyRegressor()
 # modelo_falso = modelo_falso.fit(x_treino, y_treino)
 # predicoes_falsas = modelo_falso.predict(x_teste)
-# qualidade_media1 = mean_squared_error(y_teste, predicoes_falsas)
+# qualidade_media0 = mean_squared_error(y_teste, predicoes_falsas)
 # del modelo_falso, predicoes_falsas
+# resultados['Media1: \t']=qualidade_media0
 # print(f'Tempo gasto: {time.process_time()- a} s')
 #
-# print('Modelo - Falso (mediana)')           # Mediana
+# print('\nModelo - Falso (mediana)')           # Mediana
 # a = time.process_time()
 # modelo_falso = DummyRegressor(strategy="median")
 # modelo_falso = modelo_falso.fit(x_treino, y_treino)
 # predicoes_falsas = modelo_falso.predict(x_teste)
-# qualidade_mediana1 = mean_squared_error(y_teste, predicoes_falsas)
+# qualidade_mediana0 = mean_squared_error(y_teste, predicoes_falsas)
 # del modelo_falso, predicoes_falsas
+# resultados['Mediana1: \t']=qualidade_mediana0
 # print(f'Tempo gasto: {time.process_time()- a} s')
-#
 
 
 # Nova forma de imprimir os resultados
