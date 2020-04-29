dbfd2e5c459ff8b7536bf43783b35a72401c39f4
brunoamaia94 <brunoamaia94@users.noreply.github.com>
2020-04-29 13:50:09 -0300

    metodos novos e a comparação

--- Desafio_Regressão.py
+++ Desafio_Regressão.py
@@ -13,7 +13,19 @@ from sklearn.dummy import DummyRegressor                # Modelo que retorna a m
 from sklearn.svm import LinearSVR                       # Uma forma de inteligencia artificial para regressão Lienar
 from sklearn.svm import SVR                             # Forma mais robusta e pesada das bilbiotecas usadas (não é necessariamente a melhor)
 from sklearn.tree import DecisionTreeRegressor          # Arvore de Decisão, forma mais rápida de treinar uma IA
+
 from sklearn.svm import NuSVR                           # Novo método pra testar
+
+from sklearn.linear_model import BayesianRidge
+from sklearn.linear_model import Lasso
+
+from sklearn.neighbors import KNeighborsRegressor
+
+from sklearn.ensemble import RandomForestRegressor
+
+
+
+from sklearn.neural_network import MLPRegressor         # Multi-layer Perceptron regressor
 
 # Formatações Gerais
 pd.options.display.float_format = '{:,.2f}'.format  # 2 casas decimais
@@ -51,26 +63,76 @@ y_teste = dados_teste[coluna_label].to_numpy()
 resultados  = {}    # Criar um Dicionário para armazenar a qualidade dos testes
 
 
-print('\nModelo - NuSVR')                     # NuSVR
+print('\nModelo - RandomForestRegressor')                     # RandomForestRegressor
 a = time.process_time()
-modelo_svr = NuSVR()
+modelo_svr = RandomForestRegressor()
 modelo_svr = modelo_svr.fit(x_treino, y_treino)
 predicoes_svr = modelo_svr.predict(x_teste)
 qualidade_svr0 = mean_squared_error(y_teste, predicoes_svr)
 del modelo_svr, predicoes_svr
-resultados['NuSVR: \t\t']=qualidade_svr0
+resultados['RandomForestRegressor \t\t']=qualidade_svr0
 print(f'Tempo gasto: {time.process_time()- a} s')
 
-
-print('\nModelo - SVR')                     # SVR - Sigmoid
-a = time.process_time()
-modelo_svr = SVR(kernel='sigmoid')
-modelo_svr = modelo_svr.fit(x_treino, y_treino)
-predicoes_svr = modelo_svr.predict(x_teste)
-qualidade_svr0 = mean_squared_error(y_teste, predicoes_svr)
-del modelo_svr, predicoes_svr
-resultados['Sigmoid: \t\t']=qualidade_svr0
-print(f'Tempo gasto: {time.process_time()- a} s')
+# print('\nModelo - KNeighborsRegressor')                     # KNeighborsRegressor
+# a = time.process_time()
+# modelo_svr = KNeighborsRegressor()
+# modelo_svr = modelo_svr.fit(x_treino, y_treino)
+# predicoes_svr = modelo_svr.predict(x_teste)
+# qualidade_svr0 = mean_squared_error(y_teste, predicoes_svr)
+# del modelo_svr, predicoes_svr
+# resultados['KNeighborsRegressor \t\t']=qualidade_svr0
+# print(f'Tempo gasto: {time.process_time()- a} s')
+#
+# print('\nModelo - Lasso')                     # Lasso
+# a = time.process_time()
+# modelo_svr = Lasso(alpha=2.0)
+# modelo_svr = modelo_svr.fit(x_treino, y_treino)
+# predicoes_svr = modelo_svr.predict(x_teste)
+# qualidade_svr0 = mean_squared_error(y_teste, predicoes_svr)
+# del modelo_svr, predicoes_svr
+# resultados['Lasso \t\t']=qualidade_svr0
+# print(f'Tempo gasto: {time.process_time()- a} s')
+#
+# print('\nModelo - BayesianRidge')                     # BayesianRidge
+# a = time.process_time()
+# modelo_svr = BayesianRidge(tol=0.0001)
+# modelo_svr = modelo_svr.fit(x_treino, y_treino)
+# predicoes_svr = modelo_svr.predict(x_teste)
+# qualidade_svr0 = mean_squared_error(y_teste, predicoes_svr)
+# del modelo_svr, predicoes_svr
+# resultados['BayesianRidge: \t\t']=qualidade_svr0
+# print(f'Tempo gasto: {time.process_time()- a} s')
+#
+# print('\nModelo - MLPRegressor')                     # MLPRegressor
+# a = time.process_time()
+# modelo_svr = MLPRegressor()
+# modelo_svr = modelo_svr.fit(x_treino, y_treino)
+# predicoes_svr = modelo_svr.predict(x_teste)
+# qualidade_svr0 = mean_squared_error(y_teste, predicoes_svr)
+# del modelo_svr, predicoes_svr
+# resultados['MLPRegressor: \t\t']=qualidade_svr0
+# print(f'Tempo gasto: {time.process_time()- a} s')
+#
+# print('\nModelo - NuSVR')                     # NuSVR
+# a = time.process_time()
+# modelo_svr = NuSVR()
+# modelo_svr = modelo_svr.fit(x_treino, y_treino)
+# predicoes_svr = modelo_svr.predict(x_teste)
+# qualidade_svr0 = mean_squared_error(y_teste, predicoes_svr)
+# del modelo_svr, predicoes_svr
+# resultados['NuSVR: \t\t']=qualidade_svr0
+# print(f'Tempo gasto: {time.process_time()- a} s')
+#
+#
+# print('\nModelo - SVR')                     # SVR - Sigmoid
+# a = time.process_time()
+# modelo_svr = SVR(kernel='sigmoid')
+# modelo_svr = modelo_svr.fit(x_treino, y_treino)
+# predicoes_svr = modelo_svr.predict(x_teste)
+# qualidade_svr0 = mean_squared_error(y_teste, predicoes_svr)
+# del modelo_svr, predicoes_svr
+# resultados['Sigmoid: \t\t']=qualidade_svr0
+# print(f'Tempo gasto: {time.process_time()- a} s')
 #
 # print('\nModelo - SVR')                     # SVR - precomputed
 # a = time.process_time()
@@ -161,25 +223,25 @@ print(f'Tempo gasto: {time.process_time()- a} s')
 
 
 
-##################################       Remover as notas abaxido de 100      ######################################
-del x_treino, y_treino
-f = 100 # filto do valor mínimo
-notas = dados_treino[ (dados_treino.NU_NOTA_LC > f) &         # Fazer em Apenas um comando
-                            (dados_treino.NU_NOTA_CH > f) &
-                            (dados_treino.NU_NOTA_CN > f) &
-                            (dados_treino.NU_NOTA_MT > f) &
-                            (dados_treino.NU_NOTA_REDACAO > f)]
-
-x_treino = notas[coluna_features].to_numpy()
-y_treino = notas[coluna_label].to_numpy()
-
-# sns.pairplot(notas)
-# plot.show()
-
-print('\nDados após remover notas abaixo de 100 (supondo que são Outliers')
-print(f'foram removidas {len(dados_treino) - len(notas)} linhas com notas abaixo de 100')
-print(f'Dados do Treino = {len(x_treino)} e {len(y_treino)}')
-print(f'Dados do Teste = {len(x_teste)} e {len(y_teste)}')
+# ##################################       Remover as notas abaxido de 100      ######################################
+# del x_treino, y_treino
+# f = 100 # filto do valor mínimo
+# notas = dados_treino[ (dados_treino.NU_NOTA_LC > f) &         # Fazer em Apenas um comando
+#                             (dados_treino.NU_NOTA_CH > f) &
+#                             (dados_treino.NU_NOTA_CN > f) &
+#                             (dados_treino.NU_NOTA_MT > f) &
+#                             (dados_treino.NU_NOTA_REDACAO > f)]
+#
+# x_treino = notas[coluna_features].to_numpy()
+# y_treino = notas[coluna_label].to_numpy()
+#
+# # sns.pairplot(notas)
+# # plot.show()
+#
+# print('\nDados após remover notas abaixo de 100 (supondo que são Outliers')
+# print(f'foram removidas {len(dados_treino) - len(notas)} linhas com notas abaixo de 100')
+# print(f'Dados do Treino = {len(x_treino)} e {len(y_treino)}')
+# print(f'Dados do Teste = {len(x_teste)} e {len(y_teste)}')
 
 
 # print('\nModelo - Linear SVR')              # Linear SVR
